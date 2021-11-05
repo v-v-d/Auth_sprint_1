13b9699b65f271b3581109e171afc8ae270f6326
svvladimir-ru <svetlakov454@gmail.com>
2021-11-05 22:05:06 +0300

    Подготовил модели, добавил новые зависимости в requirements

--- src/app/models.py
+++ src/app/models.py
@@ -1,11 +1,35 @@
+from flask_security import UserMixin, RoleMixin
+
 from app.database import db
 
 
-class User(db.Model):
-    # TODO Auth_sprint_1#2
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    is_staff = db.Column(db.Boolean())
+    is_active = db.Column(db.Boolean())
+    is_superuser = db.Column(db.Boolean())
+    last_login = db.Column(db.DateTime())
+    date_joined = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
 
     def __repr__(self):
-        return f"<User {self.login}>" 
+        return f"<User {self.login}>"
