402dff33e9ee7bd41121206bf642ca145701d526
v-v-d <15111988qQ>
2021-11-13 12:01:05 +0300

    #7#8#9#10 upd

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,6 @@
 import http
 
+from sqlalchemy.exc import IntegrityError
 from werkzeug import exceptions
 
 from app.api.parsers import role_parser
@@ -22,6 +23,15 @@ app.config["DEBUG"] = settings.DEBUG
 init_db(app)
 # init_api(app)
 
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security()
+
+security.init_app(app, user_datastore)
+
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
+
+
 api = Api(
     title="Auth API",
     version="1.0",
@@ -40,83 +50,135 @@ api.init_app(app)
 admin_role_schema = namespace.model("Role", role_schema)
 
 
-@namespace.route("/roles")
-class Role(Resource):
-    # method_decorators = (jwt_required(), admin_role_required)
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.id
 
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
+
+
+class BaseJWTResource(Resource):
+    method_decorators = (jwt_required(), )
+
+
+class BaseJWTAdminResource(Resource):
+    pass
+    # method_decorators = (admin_role_required, jwt_required())
+
+
+@namespace.route("/login")
+class LoginView(BaseJWTResource):
+    @namespace.doc("login")
+    @namespace.expect(role_parser)
+    @namespace.marshal_with(admin_role_schema, code=http.HTTPStatus.CREATED)
+    def post(self):
+        new_role = Role(**role_parser.parse_args())
+
+        try:
+            with session_scope() as session:
+                session.add(new_role)
+        except IntegrityError:
+            raise exceptions.BadRequest("Already exists.")
+
+        return new_role, http.HTTPStatus.CREATED
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by(username=username).one_or_none()
+    if not user or not user.check_password(password):
+        return jsonify("Wrong username or password"), 401
+
+    # Notice that we are passing in the actual sqlalchemy user object here
+    access_token = create_access_token(identity=user)
+    return jsonify(access_token=access_token)
+
+
+@app.route("/who_am_i", methods=["GET"])
+@jwt_required()
+def protected():
+    # We can now access our sqlalchemy User object via `current_user`.
+    return jsonify(
+        id=current_user.id,
+        full_name=current_user.full_name,
+        username=current_user.username,
+    )
+
+
+@namespace.route("/roles")
+class RolesView(BaseJWTAdminResource):
     @namespace.doc("get list of roles")
     @namespace.marshal_with(admin_role_schema, as_list=True, code=http.HTTPStatus.OK)
-    # @jwt_required()
-    # @admin_role_required
     def get(self):
         return Role.query.all()
 
-#
-#
-# @namespace.route("/roles")
-# class Role(Resource):
-#     method_decorators = (jwt_required(), admin_role_required)
-#
-#     @namespace.doc("get list of roles")
-#     @namespace.marshal_with(admin_role_schema, as_list=True, code=http.HTTPStatus.OK)
-#     # @jwt_required()
-#     # @admin_role_required
-#     def get(self):
-#         return Role.query.all()
-#
-#     @namespace.doc("create role")
-#     @namespace.expect(role_parser)
-#     @namespace.marshal_with(admin_role_schema, code=http.HTTPStatus.CREATED)
-#     # @jwt_required()
-#     # @admin_role_required
-#     def post(self):
-#         with session_scope() as session:
-#             try:
-#                 new_role = Role(**role_parser.parse_args())
-#             except Exception:
-#                 raise exceptions.BadRequest("Already exists.")
-#
-#             session.add(new_role)
-#
-#         return new_role
-#
-#     @namespace.doc("change role")
-#     @namespace.expect(role_parser)
-#     @namespace.marshal_with(admin_role_schema, code=http.HTTPStatus.OK)
-#     # @jwt_required()
-#     # @admin_role_required
-#     def patch(self, role_id: int):
-#         with session_scope():
-#             role = Role.query.get_or_404(id=role_id)
-#
-#             if role.name in Role.Meta.PROTECTED_ROLE_NAMES:
-#                 raise exceptions.BadRequest("This role is protected.")
-#
-#             role.update_or_skip(**role_parser.parse_args())
-#
-#         return role
-#
-#     @namespace.doc("delete role")
-#     @namespace.marshal_with("", code=http.HTTPStatus.NO_CONTENT)
-#     # @jwt_required()
-#     # @admin_role_required
-#     def delete(self, role_id: int):
-#         with session_scope():
-#             role = Role.query.get_or_404(id=role_id)
-#
-#             if role.name in Role.Meta.PROTECTED_ROLE_NAMES:
-#                 raise exceptions.BadRequest("This role is protected.")
-#
-#             role.is_active = False
+    @namespace.doc("create role", responses={
+        http.HTTPStatus.BAD_REQUEST: "Bad Request",
+        http.HTTPStatus.CREATED: "Created",
+    })
+    @namespace.expect(role_parser)
+    @namespace.marshal_with(admin_role_schema, code=http.HTTPStatus.CREATED)
+    def post(self):
+        new_role = Role(**role_parser.parse_args())
+
+        try:
+            with session_scope() as session:
+                session.add(new_role)
+        except IntegrityError:
+            raise exceptions.BadRequest("Already exists.")
+
+        return new_role, http.HTTPStatus.CREATED
 
 
+@namespace.route("/roles/<int:role_id>")
+class SpecificRolesView(BaseJWTAdminResource):
+    @namespace.doc("change role", responses={
+        http.HTTPStatus.NOT_FOUND: "Not Found",
+        http.HTTPStatus.BAD_REQUEST: "Bad Request",
+    })
+    @namespace.expect(role_parser)
+    @namespace.marshal_with(admin_role_schema, code=http.HTTPStatus.OK)
+    def patch(self, role_id: int):
+        role = Role.query.get_or_404(id=role_id)
 
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security()
+        args = role_parser.parse_args()
 
-security.init_app(app, user_datastore)
+        if args["name"] in Role.Meta.PROTECTED_ROLE_NAMES:
+            raise exceptions.BadRequest("This role is protected.")
 
-app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
-jwt = JWTManager(app)
+        try:
+            with session_scope():
+                role.update_or_skip(**role_parser.parse_args())
+        except IntegrityError:
+            raise exceptions.BadRequest("Already exists.")
+
+        return role
+
+    @namespace.doc("delete role", responses={
+        http.HTTPStatus.NOT_FOUND: "Not Found",
+        http.HTTPStatus.BAD_REQUEST: "Bad Request",
+        http.HTTPStatus.NO_CONTENT: "No Content",
+    })
+    @namespace.marshal_with("", code=http.HTTPStatus.NO_CONTENT)
+    def delete(self, role_id: int):
+        role = Role.query.get_or_404(id=role_id)
+
+        args = role_parser.parse_args()
+
+        if args["name"] in Role.Meta.PROTECTED_ROLE_NAMES:
+            raise exceptions.BadRequest("This role is protected.")
+
+        with session_scope():
+            role.is_active = False
+
+        return "", http.HTTPStatus.NO_CONTENT
+
 
 app.app_context().push()
