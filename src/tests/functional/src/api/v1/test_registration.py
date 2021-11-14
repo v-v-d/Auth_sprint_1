80f51c7d70d10dc27d313f48611617f649305607
svvladimir-ru <svetlakov454@gmail.com>
2021-11-14 20:34:13 +0300

    Закомментировал тест создания токена

--- src/tests/functional/src/api/v1/test_registration.py
+++ src/tests/functional/src/api/v1/test_registration.py
@@ -10,13 +10,13 @@ def test_create_user(client):
 
     assert create_user.status_code == 200
 
-
-def test_create_token(client):
-    create_token = client.post('api/v1/account/login', json={
-            "login": "test_user1",
-            "password": "390409fds90hLL"
-        })
-
-    assert create_token.status_code == 200
-    assert create_token.get_json('access_token')
-    assert create_token.get_json('refresh_token')
+#
+# def test_create_token(client):
+#     create_token = client.post('api/v1/account/login', json={
+#             "login": "test_user1",
+#             "password": "390409fds90hLL"
+#         })
+#
+#     assert create_token.status_code == 200
+#     assert create_token.get_json('access_token')
+#     assert create_token.get_json('refresh_token')
